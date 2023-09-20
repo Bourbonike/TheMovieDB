706901f3256993315d2a1ce8900dadc81e7c77d2
Bourbonike <100319658+Bourbonike@users.noreply.github.com>
2023-09-21 00:31:35 +0300

    AND-11 (#2)
    Добавить ViewModel для экрана деталей.
    Добавить data class содержащий данные о фильме (заголовок и описание)

--- app/src/main/java/com/example/themoviedp/DetailsActivity.kt
+++ app/src/main/java/com/example/themoviedp/DetailsActivity.kt
@@ -3,18 +3,34 @@ package com.example.themoviedp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.themoviedp.databinding.ActivityDetailsScreenBinding
+import kotlinx.coroutines.launch
 
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsScreenBinding
+
+    private val viewModel: DetailsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details_screen)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Cinema"
-    }
+        binding = ActivityDetailsScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.e("AAA", "Activity created")
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) finish()
-        return true
+        binding.button.setOnClickListener { viewModel.changeMovie() }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.movieState.collect { movieState ->
+                    binding.toolbar.title = movieState.title
+                    binding.textView2.text = movieState.description
+                }
+            }
+        }
     }
 }
